989153548eecb15bd8491be87d34cc1ee12ae115
shohreh.zarkesh <shohreh.zarkesh@gmail.com>
2023-02-07 19:45:11 +0330

    init : add ionic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+//react and ionic
+import { Redirect, Route } from "react-router-dom";
+import { IonReactRouter } from "@ionic/react-router";
+import { setupIonicReact } from "@ionic/react";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+//ionic components
+import { IonApp, IonRouterOutlet } from "@ionic/react";
+
+import { IonButton} from "@ionic/react";
+
+
+setupIonicReact();
 
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+      <IonApp>
+          <IonButton> Hello ionic</IonButton>
+      </IonApp>
   );
 }
-
 export default App;
