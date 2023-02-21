1bfda4af42578ee2a5fbe105814a2d2b9f7e553a
shohreh.zarkesh <shohreh.zarkesh@gmail.com>
2023-02-21 19:59:35 +0330

    feat : add back button in result

--- src/screens/Home/screens/Results/index.jsx
+++ src/screens/Home/screens/Results/index.jsx
@@ -1,6 +1,16 @@
-export function  ResultsScreen(){
+import {IonButtons, IonPage, IonToolbar} from "@ionic/react";
+import {BackButton} from "../../../../components/BackButton";
+
+export function ResultsScreen() {
 
     return (<>
         you are in Result
+        <IonPage>
+            <IonToolbar>
+                <IonButtons slot="start">
+                    <BackButton/>
+                </IonButtons>
+            </IonToolbar>
+        </IonPage>
     </>)
 }
